cf2fcb134687acdf5668c8d9c5a20bc8916e91fd
ArinaKumeisha <arinanozdracheva@mail.ru>
2021-07-01 17:49:29 +0300

    5 commit

---
+++ src/05/07-destructuring-asigment/08-associativnyj-massiv/08.01.test.ts
@@ -0,0 +1,50 @@
+import {userObj} from "./08.01";
+
+
+type UsersType = {
+    [key: string]: { id: number, name: string }
+}
+
+
+let users: UsersType;
+beforeEach(() => {
+        users = {      //ассоциативный массив- достаем объект по ключу
+            "101": {id: 101, name: "Arina"},
+            "1234": {id: 1234, name: "Alex"},
+            "14": {id: 14, name: "Dasha"},
+            "1": {id: 1, name: "Tanya"},
+        }
+    }
+)
+
+/*let user={id:444, name: "Anna"}
+users[user.id]=user
+delete users[user.id]
+users[user.id].name="Alla"*/
+
+test("should update corresponding user", () => {
+    expect(users["1"]).toBe("Tanya")
+    expect(users["101"]).toBe("Arina")
+    expect(users["14"]).toBe("Dasha")
+
+})
+
+test("update name", () => {
+    users["1"].name = "Tatyana"
+    expect(users["1"].name).toBe("Tatyana")
+})
+
+
+test("should be corresponding user from obj", () => {
+    expect(userObj[0]).toBe("Arina")
+    expect(userObj[1]).toBe("Alex")
+    expect(userObj[2]).toBe("Dasha")
+    expect(userObj[3]).toBe("Tanya")
+})
+
+test("should be corresponding users", () => {
+    expect(users["101"].id).toBe(101)
+    expect(users["1234"].name).toBe("Alex")
+    expect(users["14"].name).toBe("Dasha")
+    expect(users["1"].id).toBe(1)
+})
---
+++ src/05/07-destructuring-asigment/08-associativnyj-massiv/08.01.ts
@@ -0,0 +1,18 @@
+export const userArray = ["Arina", "Alex", "Dasha", "Tanya"]
+
+export const userObj = {
+    "0": "Arina",
+    "1": "Alex",
+    "2": "Dasha",
+    "3": "Tanya",
+}
+
+
+export const usersArray = [
+    {id: 101, name: "Arina"},
+    {id: 1234, name: "Alex"},
+    {id: 14, name: "Dasha"},
+    {id: 1, name: "Tanya"},
+]
+
+
